b2a2acf9f6c2b7a4745899ef0cc384388da6b244
pkwtz <keattisak.t@ku.th>
2019-12-01 12:50:04 +0700

    แก้บัค add newTransaction

--- routes/sale.js
+++ routes/sale.js
@@ -1,63 +1,63 @@
 const router = require('express').Router();
-var mongoose = require('mongoose');
 let User = require('../models/user.model');
 let Transaction = require('../models/transaction.model');
 let Block = require('../models/block.model');
 
 
-router.route('/add').post((req, res) => {
+router.route('/add').post(async (req, res) => {
     const firstname = req.body.destination.split(' ')[0]
     const lastname = req.body.destination.split(' ')[1]
-
-    if (User.findOne({firstname, lastname})) {
-            let transaction = {
-                source: req.body.source,
-                rubberType: req.body.rubberType,
-                volume: req.body.volume,
-                price: req.body.price,
-                destination: req.body.destination,
-            } 
-            
-            const newTransaction = new Transaction(transaction)
-            const lastState = newTransaction.destination
-            const rubberType = newTransaction.rubberType
-            newTransaction.save()
-                .then(() => {
-                    Transaction.findOne({rubberType, destination: req.body.source})
-                        .then(transaction => {
-                            // console.log(transaction)
-                            if(transaction != null) {
-                                // console.log('Update Block ', rubberType)
-                                // console.log(transaction.source, ' -> ', transaction.destination)
-                                Block.find({lastState: transaction.destination, rubberType})
-                                .then(blocks => {
-                                    // console.log(blocks)
-                                    blocks.map(obj => {
-                                        obj.holders.push(newTransaction.destination)
-                                        obj.chain.push(newTransaction)
-                                        obj.lastState = newTransaction.destination
-                                        obj.save()
-                                    })
+    const source = await User.findOne({firstname: req.body.source.split(' ')[0], lastname: req.body.source.split(' ')[1]})
+    const destination = await User.findOne({firstname, lastname})
+    // if source and destination are exist
+    if (source && destination) {
+        let transaction = {
+            source: req.body.source,
+            rubberType: req.body.rubberType,
+            volume: req.body.volume,
+            price: req.body.price,
+            destination: req.body.destination,
+        } 
+        
+        const newTransaction = new Transaction(transaction)
+        const lastState = newTransaction.destination
+        const rubberType = newTransaction.rubberType
+        newTransaction.save()
+            .then(() => {
+                Transaction.findOne({rubberType, destination: req.body.source})
+                    .then(transaction => {
+                        // console.log(transaction)
+                        if(transaction != null) {
+                            // console.log('Update Block ', rubberType)
+                            // console.log(transaction.source, ' -> ', transaction.destination)
+                            Block.find({lastState: transaction.destination, rubberType})
+                            .then(blocks => {
+                                // console.log(blocks)
+                                blocks.map(obj => {
+                                    obj.holders.push(newTransaction.destination)
+                                    obj.chain.push(newTransaction)
+                                    obj.lastState = newTransaction.destination
+                                    obj.save()
                                 })
-                            } else {
-                                // console.log('new Block')
-                                let chain = []
-                                let holders = []
-                                chain.push(newTransaction)
-                                if(!holders.includes(newTransaction.source)){
-                                    holders.push(newTransaction.source)
-                                }
-                                if(!holders.includes(newTransaction.destination)){
-                                    holders.push(newTransaction.destination)
-                                }
-                                const newBlock = new Block({lastState, rubberType, chain, holders})
-                                newBlock.save()
+                            })
+                        } else {
+                            // console.log('new Block')
+                            let chain = []
+                            let holders = []
+                            chain.push(newTransaction)
+                            if(!holders.includes(newTransaction.source)){
+                                holders.push(newTransaction.source)
                             }
-                            res.json('Transaction added!')
-                        })
+                            if(!holders.includes(newTransaction.destination)){
+                                holders.push(newTransaction.destination)
+                            }
+                            const newBlock = new Block({lastState, rubberType, chain, holders})
+                            newBlock.save()
+                        }
+                        res.json('Transaction added!')
                     })
-                .catch(err => res.status(400).json('Error: ' + err))
-
+                })
+            .catch(err => res.status(400).json('Error: ' + err))
     } else {
         res.status(400).json('ไม่มีผู้ใช่นี้ในระบบ')
     }
