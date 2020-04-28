162c8605079828d9e8281f8da20d097261151486
pk <keattisak.t@ku.th>
2020-04-28 16:10:54 +0700

    finally edit login response

--- routes/users.js
+++ routes/users.js
@@ -65,13 +65,13 @@ router.route('/login').post(async (req, res) => {
         .then(user => {
             if(bcrypt.compareSync(req.body.password, user.password)){
                 const token = jwt.sign({username: user.username, role: user.role}, process.env.ACCESS_TOKEN_PRIVATE)
-                const { role, gardens, friendlist, _id, username, firstname, lastname, cert_1, address, district, subdistrict, zipcode, province, citizen_id, email } = user._doc
+                const { role, gardens, friendlist, _id, username, firstname, lastname, cert_1, address, district, subdistrict, zipcode, province, citizen_id, email, tel } = user._doc
                 const payload = {
                     username, role, _id, email,
                     address, district, subdistrict,
                     zipcode, province,citizen_id,
                     firstname, lastname, friendlist,
-                    gardens, cert_1, token
+                    gardens, cert_1, token, tel
                 }
                 res.json({message:"Logged In", user: payload})
             } else {
